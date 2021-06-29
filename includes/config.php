e687f68df475c6b59fe85999af32466c96e16014
Matteo Sacchi <matteoseik@hotmail.it>
2021-06-29 11:06:54 +0200

    Updates

--- includes/config.php
+++ includes/config.php
@@ -1,7 +1,7 @@
 <?php
 
     $pageUrl = strtok( (isset($_SERVER['HTTP_X_FORWARDED_PROTO']) || isset($_SERVER['HTTPS']) ? "https" : "http") . "://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]", "?");
-    $pageTitle = "Il cantiere dei cittadini digitali ";
+    $pageTitle = "Il cantiere dei cittadini digitali";
     $pageDescription = "Una grande alleanza guidata da TIM per diffondere le competenze tecnologiche nel nostro Paese insieme ad aziende pubbliche, private e no profit.";
 
     $twitterString = "#digitaltransformation" . $pageUrl;
