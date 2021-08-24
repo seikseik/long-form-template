f7fe7e704c7b8654a7874789c919ccd1aa6e7975
Matteo Sacchi <matteoseik@hotmail.it>
2021-08-24 14:43:39 +0200

    image + draggable update

--- src/includes/config.php
+++ src/includes/config.php
@@ -1,9 +1,9 @@
 <?php
 
     $pageUrl = strtok( (isset($_SERVER['HTTP_X_FORWARDED_PROTO']) || isset($_SERVER['HTTPS']) ? "https" : "http") . "://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]", "?");
-    $pageTitle = "Il cantiere dei cittadini digitali ";
-    $pageDescription = "Una grande alleanza guidata da TIM per diffondere le competenze tecnologiche nel nostro Paese insieme ad aziende pubbliche, private e no profit.";
+    $pageTitle = "Creatività e spirito di gruppo: così la scuola valorizza l’arte";
+    $pageDescription = "Partecipando al concorso “Cento di questi anni” promosso da Fila – Fabbrica Italiana Lapis ed Affini, studenti e insegnanti di tutta Italia si sono confrontati sulle opere del Novecento, reinterpretandole secondo le proprie sensibilità.";
 
-    $twitterString = "#digitaltransformation" . $pageUrl;
+    $twitterString = " " . $pageUrl;
     $whatsappString = $pageTitle. " " . $pageUrl;
 ?>
