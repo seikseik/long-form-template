112c45aea0978cc65a72bdb900b4a00652da2317
seikseik <matteoseik@hotmail.it>
2021-09-15 12:02:08 +0000

    Deploying to gh-pages from @ seikseik/long-form-template@e7bca632c676857a0678fda819364049eaf0aaea 🚀

--- includes/config.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-    $pageUrl = strtok( (isset($_SERVER['HTTP_X_FORWARDED_PROTO']) || isset($_SERVER['HTTPS']) ? "https" : "http") . "://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]", "?");
-    $pageTitle = "Creatività e spirito di gruppo: così la scuola valorizza l’arte";
-    $pageDescription = "Partecipando al concorso “Cento di questi anni” promosso da Fila – Fabbrica Italiana Lapis ed Affini, studenti e insegnanti di tutta Italia si sono confrontati sulle opere del Novecento, reinterpretandole secondo le proprie sensibilità.";
-
-    $twitterString = " " . $pageUrl;
-    $whatsappString = $pageTitle. " " . $pageUrl;
-?>
