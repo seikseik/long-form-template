81ae362a4bc9862ea3b3c39214a5dc5b97fff887
Matteo Sacchi <matteoseik@hotmail.it>
2021-09-24 16:15:39 +0200

    fix animazioni

--- src/js/animation.js
+++ src/js/animation.js
@@ -1,64 +1,62 @@
 import { gsap } from "gsap";
 import { ScrollToPlugin } from "gsap/ScrollToPlugin";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
-import { SplitText } from "gsap/SplitText";
 
 
-  import lozad from 'lozad'
+gsap.registerPlugin(ScrollToPlugin, ScrollTrigger);
 
-  const observer = lozad();
-  observer.observe();
 
-  gsap.registerPlugin(ScrollToPlugin, ScrollTrigger, SplitText);
+let imagHero =  document.querySelector(".image-hero");
+let title = document.querySelector(".title h1");
+let subtitle = document.querySelector(".title h3");
+let vLine = document.querySelector(".title .vertical-line");
+let summary = document.querySelector(".hero-description");
+let heroContainer = document.querySelector(".hero-text-container");
+
+let heroTl =  gsap.timeline();
+heroTl.fromTo(imagHero,{autoAlpha: 0},{  duration: 1.5, autoAlpha: 1});
+heroTl.fromTo(heroContainer,{autoAlpha: 0},{  duration: 1.5, autoAlpha: 1, delay: -1});
+gsap.set(vLine, {transformOrigin:"bottom"})
+gsap.fromTo(vLine,{scaleY: 0}, {duration: 0.7, scaleY: 1, delay: 0.7, ease: "circ.out",});
+
 
 
 let menuBtn = document.querySelector(".menu");
-menu-
-
-  // animation hero text
-  // const heroTitle = document.querySelector(".animation-text-hero");
-  //
-  // let tl = gsap.timeline(),
-  //   mySplitText = new SplitText(heroTitle, {type:"words,chars", wordsClass: "split-line"
-  // }),
-  // chars = mySplitText.chars;
-  // gsap.set(heroTitle, {perspective: 400});
-  // tl.fromTo(chars, {autoAlpha: 0},
-  //         {  duration: 1,
-  //              autoAlpha: 1,
-  //              delay: 0.1,
-  //              ease: "circ.out",
-  //              stagger: 0.095,
-  //         },
-  //         "+=0");
-
-// animation quotes
-
-  // const quotes = document.querySelectorAll(".animation-text");
-  //
-  //   quotes.forEach((quote, i) => {
-  //     quote.split = new SplitText(quote, {
-  //       type:"words,chars",
-  //       wordsClass: "split-line"
-  //     });
-  //     gsap.set(quote, {perspective: 400});
-  //     const anim = gsap.fromTo(quote.split.chars,
-  //     {autoAlpha: 0},{  duration: 1, autoAlpha: 1,ease: "circ.out",stagger: 0.045, delay: 0.2});
-  //
-  //     ScrollTrigger.create({
-  //       trigger: quote,
-  //       animation: anim,
-  //       toggleActions: 'play none none none',
-  //       once: true,
-  //     });
-  //   });
+let menu =  document.querySelector(".menu-full");
+let menuItems = gsap.utils.toArray(".menu-item-container");
+menuBtn.onclick = function(){
+    if(menuBtn.classList.contains("active")){
+      let tl = gsap.timeline();
+        tl.fromTo(menuItems,{autoAlpha: 1},{  duration: 0.2, autoAlpha: 0 ,stagger: -0.1, delay: 0.1});
+        tl.fromTo(menu,{autoAlpha: 1},{  duration: 0.3, autoAlpha: 0, ease: "circ.out"});
+        tl.set(menu, {visibility: "hidden"});
+        menuBtn.classList.remove("active")
+    }else{
+      menu.visibility = "visible";
+      gsap.fromTo(menu,{autoAlpha: 0},{  duration: 0.35, autoAlpha: 1,ease: "circ.out"});
+      gsap.fromTo(menuItems,{autoAlpha: 0},{  duration: 1, autoAlpha: 1 ,stagger: 0.2, delay: 0.35});
+      menuBtn.classList.add("active")
+    }
+  };
 
 
 
   // fade in
   const fade = gsap.utils.toArray("[fade]");
   fade.forEach((el, i) => {
-    const anim = gsap.fromTo(el, {autoAlpha: 0}, {duration: 1, autoAlpha: 1});
+    const anim = gsap.fromTo(el, {autoAlpha: 0}, {duration: 1.5, autoAlpha: 1});
+    ScrollTrigger.create({
+      trigger: el,
+      animation: anim,
+      toggleActions: 'play none none none',
+      once: true,
+    });
+  });
+
+  // fade up
+  const fadeUp = gsap.utils.toArray("[fade-up]");
+  fadeUp.forEach((el, i) => {
+    const anim = gsap.fromTo(el, {autoAlpha: 0, y:0}, {duration: 1, autoAlpha: 1, y: -15});
     ScrollTrigger.create({
       trigger: el,
       animation: anim,
@@ -68,36 +66,16 @@ menu-
   });
 
 
-
   // hr line animation
-  // const hr = gsap.utils.toArray(".hr");
-  //
-  // hr.forEach((el, i) => {
-  //   gsap.set(el, {transformOrigin:"left"})
-  //   const anim = gsap.fromTo(el, {scaleX: 0}, {duration: .5, scaleX: 1});
-  //   ScrollTrigger.create({
-  //     trigger: el,
-  //     animation: anim,
-  //     ease: "circ.out",
-  //     toggleActions: 'play none none none',
-  //     once: true,
-  //   });
-  // });
-
-
-// swiper
-// let swipers = document.querySelectorAll(".swiper-container")
-// swipers.forEach((item, i) => {
-//   const swiper = new Swiper(item, {
-//     navigation: {
-//     nextEl: '.swiper-button-next',
-//     prevEl: '.swiper-button-prev',
-//     },
-//     slidesPerView: 1,
-//     spaceBetween: 30,
-//     centeredSlides: true,
-//     preloadImages: false,
-//     lazy: true,
-//     autoHeight: true,
-//   });
-// });
+  const hr = gsap.utils.toArray(".hr");
+  hr.forEach((el, i) => {
+    gsap.set(el, {transformOrigin:"left"})
+    const anim = gsap.fromTo(el, {scaleX: 0}, {duration: 0.9, scaleX: 1, delay: .3, ease: "circ.out"});
+    ScrollTrigger.create({
+      trigger: el,
+      animation: anim,
+      ease: "circ.out",
+      toggleActions: 'play none none none',
+      once: true,
+    });
+  });
