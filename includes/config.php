d53bb1ddea0073652999a1786b039603ff27e891
seikseik <matteoseik@hotmail.it>
2021-09-29 13:29:36 +0000

    Deploying to gh-pages from @ seikseik/long-form-template@4adc47830227d34c6674d48c28e921265d30ab71 🚀

--- includes/config.php
+++ includes/config.php
@@ -2,7 +2,7 @@
 
     $pageUrl = strtok( (isset($_SERVER['HTTP_X_FORWARDED_PROTO']) || isset($_SERVER['HTTPS']) ? "https" : "http") . "://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]", "?");
     $pageTitle = "Serpenti: un emblema tra mito e glamour";
-    $pageDescription = "";
+    $pageDescription = "Il segno più iconico della creatività di Bulgari cambia, senza mai perdere appeal e potere di seduzione, da oltre settant'anni.";
 
     $twitterString = " " . $pageUrl;
     $whatsappString = $pageTitle. " " . $pageUrl;
