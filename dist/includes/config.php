81f58703d0d25826ff52a091b8eb2b74ee4a7602
Matteo Sacchi <matteoseik@hotmail.it>
2021-10-22 15:36:32 +0200

    build#4

--- dist/includes/config.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-    $pageUrl = strtok( (isset($_SERVER['HTTP_X_FORWARDED_PROTO']) || isset($_SERVER['HTTPS']) ? "https" : "http") . "://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]", "?");
-    $pageTitle = "Serpenti: un emblema tra mito e glamour";
-    $pageDescription = "Il segno più iconico della creatività di Bulgari cambia, senza mai perdere appeal e potere di seduzione, da oltre settant'anni.";
-
-    $twitterString = "Serpenti: un emblema tra mito e glamour %23bulgari %23bulgariserpenti %23Bulgarijewelry " . $pageUrl;
-    $whatsappString = $pageTitle. " " . $pageUrl;
-?>
