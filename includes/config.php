78826f6e2ec20b5cde5e6915e4b4b21b1f1e914b
seikseik <matteoseik@hotmail.it>
2021-10-27 12:58:21 +0000

    Deploying to gh-pages from @ seikseik/long-form-template@ac22db07d82c82be07df7804429d98398c840094 🚀

--- includes/config.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-    $pageUrl = strtok( (isset($_SERVER['HTTP_X_FORWARDED_PROTO']) || isset($_SERVER['HTTPS']) ? "https" : "http") . "://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]", "?");
-    $pageTitle = "Il parco va in città";
-    $pageDescription = "Un parco nel cuore della città con fioriere e orti urbani, dove fare sport con attrezzature e app intelligenti, riposare su panchine di design e ammirare qualche opera d’arte urbana.";
-
-    $twitterString = " " . $pageUrl;
-    $whatsappString = $pageTitle. " " . $pageUrl;
-?>
