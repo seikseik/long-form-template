af8c98d6a2cb49a77df566ac3e837512bd7599c0
Matteo Sacchi <matteoseik@hotmail.it>
2021-10-27 11:25:45 +0200

    agos deploy 2

--- src/includes/config.php
+++ src/includes/config.php
@@ -1,8 +1,8 @@
 <?php
 
     $pageUrl = strtok( (isset($_SERVER['HTTP_X_FORWARDED_PROTO']) || isset($_SERVER['HTTPS']) ? "https" : "http") . "://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]", "?");
-    $pageTitle = "Creatività e spirito di gruppo: così la scuola valorizza l’arte";
-    $pageDescription = "Partecipando al concorso “Cento di questi anni” promosso da Fila – Fabbrica Italiana Lapis ed Affini, studenti e insegnanti di tutta Italia si sono confrontati sulle opere del Novecento, reinterpretandole secondo le proprie sensibilità.";
+    $pageTitle = "Il parco va in città";
+    $pageDescription = "Un parco nel cuore della città con fioriere e orti urbani, dove fare sport con attrezzature e app intelligenti, riposare su panchine di design e ammirare qualche opera d’arte urbana.";
 
     $twitterString = " " . $pageUrl;
     $whatsappString = $pageTitle. " " . $pageUrl;
