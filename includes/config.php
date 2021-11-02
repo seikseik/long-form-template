92fda4c19896f219648e5722d085d1c13c6d16a4
seikseik <matteoseik@hotmail.it>
2021-11-02 15:33:25 +0000

    Deploying to gh-pages from @ seikseik/long-form-template@f665a5f88269b0740523b3ca90d3a694c3750175 🚀

--- includes/config.php
+++ includes/config.php
@@ -1,8 +1,8 @@
 <?php
 
     $pageUrl = strtok( (isset($_SERVER['HTTP_X_FORWARDED_PROTO']) || isset($_SERVER['HTTPS']) ? "https" : "http") . "://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]", "?");
-    $pageTitle = "La nuova rete del calcio";
-    $pageDescription = "Lo sport in tv sta vivendo una transizione epocale, spostandosi dal satellite allo streaming. Ora, grazie a TimVision, i tifosi hanno lo stadio in tasca";
+    $pageTitle = "Il parco va in città";
+    $pageDescription = "Un parco nel cuore della città con fioriere e orti urbani, dove fare sport con attrezzature e app intelligenti, riposare su panchine di design e ammirare qualche opera d’arte urbana.";
 
     $twitterString = " " . $pageUrl;
     $whatsappString = $pageTitle. " " . $pageUrl;
