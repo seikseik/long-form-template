66677f1b35bc0caacc47a2d0e1a288833e6cf790
seikseik <matteoseik@hotmail.it>
2021-12-20 13:44:32 +0000

    Deploying to gh-pages from @ seikseik/long-form-template@6e514b60c479427d7738065c7d91a489792177c1 🚀

--- includes/config.php
+++ includes/config.php
@@ -1,9 +1,9 @@
 <?php
 
     $pageUrl = strtok( (isset($_SERVER['HTTP_X_FORWARDED_PROTO']) || isset($_SERVER['HTTPS']) ? "https" : "http") . "://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]", "?");
-    $pageTitle = "Il parco va in città";
-    $pageDescription = "Un parco nel cuore della città con fioriere e orti urbani, dove fare sport con attrezzature e app intelligenti, riposare su panchine di design e ammirare qualche opera d’arte urbana.";
+    $pageTitle = "Serpenti: un emblema tra mito e glamour";
+    $pageDescription = "Il segno più iconico della creatività di Bulgari cambia, senza mai perdere appeal e potere di seduzione, da oltre settant'anni.";
 
-    $twitterString = " " . $pageUrl;
+    $twitterString = "#bulgari #bulgariserpenti #Bulgarijewelry" . $pageUrl;
     $whatsappString = $pageTitle. " " . $pageUrl;
 ?>
