c4f8194c94f06894797777fc8a2bba28c38835b2
Matteo Sacchi <matteoseik@hotmail.it>
2022-01-03 17:05:28 +0100

    deploy

--- src/js/simulatore.js
+++ src/js/simulatore.js
@@ -4,10 +4,7 @@ let titolo = document.querySelector(".title-presidente")
 let txt = document.querySelector(".text-result")
 
 window.addEventListener('electionWidgetResult', function (e) {
-   console.log(e.detail)
    let img = e.detail.titolo
-   // img = img.replace(" ", "_");
-   // console.log(img);
    immagine.src = `../presidenti/${img}.png`
    scenario.innerHTML = e.detail.occhiello
    titolo.innerHTML = e.detail.titolo
