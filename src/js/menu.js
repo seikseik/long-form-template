aaa97bd32bca878769648e860e78ace56e0e4c50
Matteo Sacchi <matteoseik@hotmail.it>
2022-01-12 12:15:58 +0100

    fix

--- src/js/menu.js
+++ src/js/menu.js
@@ -8,38 +8,11 @@ gsap.registerPlugin(ScrollToPlugin, ScrollTrigger, SplitText);
 let body = document.querySelector("body");
 
 let menuBtn = document.querySelector("#menu-btn");
-var burger = gsap.timeline({
-    paused: true
-});
 
-burger
-    .to(" .burger-one", 0.25, { y: "5px", ease: "circ.out" }, "burg")
-    .to(" .burger-tree", 0.25, { y: "-5px", ease: "circ.out" }, "burg")
-    .to(
-        " .burger-two",
-        0.25,
-        { scale: 0.1, transformOrigin: "center", ease: "circ.out" },
-        "burg"
-    )
-    .add("rotate")
-    .to(" .burger-one", 0.25, { y: "10", ease: "circ.out" }, "rotate")
-    .to(" .burger-tree", 0.25, { y: "-10", ease: "circ.out" }, "rotate")
-    .to(
-        " .burger-one",
-        0.25,
-        { rotationZ: 45, transformOrigin: "50% 50%", ease: "circ.out" },
-        "rotate"
-    )
-    .to(
-        " .burger-tree",
-        0.25,
-        { rotationZ: -45, transformOrigin: "50% 50%", ease: "circ.out" },
-        "rotate"
-    );
+let menu =  document.querySelector(".menu-full");
+let menuItems = gsap.utils.toArray(".menu-item-container");
+let flag = false;
 
-    let menu =  document.querySelector(".menu-full");
-    let menuItems = gsap.utils.toArray(".menu-item-container");
-    let flag = false;
 
 let scroll = false;
 menuBtn.addEventListener("click", () => {
@@ -50,7 +23,8 @@ menuBtn.addEventListener("click", () => {
             gsap.fromTo(menu,{autoAlpha: 0},{  duration: 0.3, autoAlpha: 1, ease: "circ.out"});
             gsap.fromTo(menuItems,{autoAlpha: 0},{  duration: 0.2, autoAlpha: 1, stagger: 0.2, delay: 0.2});
             body.style.overflowY = "hidden"
-            burger.restart();
+            menuBtn.classList.add("active")
+
         } else {
             let tl = gsap.timeline({
                 paused: true
@@ -60,7 +34,7 @@ menuBtn.addEventListener("click", () => {
               tl.set(menu, {visibility: "hidden"});
               tl.restart();
               body.style.overflowY = "scroll";
-              burger.reverse();
+              menuBtn.classList.remove("active")
         }
 });
 
