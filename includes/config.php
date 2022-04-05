94c9b5dc12e24735070426958b0c870608a0d067
seikseik <matteoseik@hotmail.it>
2022-04-05 08:29:54 +0000

    Deploying to gh-pages from @ seikseik/long-form-template@50a8933cf3fc4bc263a3284e4a4350c3a084d2b4 🚀

--- includes/config.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-    $pageUrl = strtok( (isset($_SERVER['HTTP_X_FORWARDED_PROTO']) || isset($_SERVER['HTTPS']) ? "https" : "http") . "://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]", "?");
-    $pageTitle = "Il nuovo volto della boutique di Milano";
-    $pageDescription = "Al centro del restyling, a cui ha contribuito anche l'architetto Stefano Boeri, un omaggio alla città e all'eleganza dello stile italiano. Tra cenni al passato e codici contemporanei";
-
-    $twitterString = "" . $pageUrl;
-    $whatsappString = $pageTitle. " " . $pageUrl;
-?>
