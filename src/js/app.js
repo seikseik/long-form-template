c7e891343c946c231a4a7ffb842901bbf87c7d07
Matteo Sacchi <matteoseik@hotmail.it>
2022-06-14 11:43:34 +0200

    deploy

--- src/js/app.js
+++ src/js/app.js
@@ -1,4 +1,7 @@
-import loop from '../images/content/CirkelBroen_15Sek_02.mp4';
+import video1 from '../images/content/video_1.mp4';
+import video2 from '../images/content/video_2.mp4';
+import video3 from '../images/content/video_3.mp4';
+import video4 from '../images/content/video_4.mp4';
 
 import './animations';
 
