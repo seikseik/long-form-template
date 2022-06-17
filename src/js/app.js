f5dec0a60a94fef02e7f1ee9cb2243eb4b6c4833
Matteo Sacchi <matteoseik@hotmail.it>
2022-06-17 10:54:36 +0200

    deploy

--- src/js/app.js
+++ src/js/app.js
@@ -1,6 +1,5 @@
 import video1 from '../images/content/video_1.mp4';
 import video2 from '../images/content/video_2.mp4';
-import video3 from '../images/content/video_3.mp4';
 import video4 from '../images/content/video_4.mp4';
 
 import './animations';
