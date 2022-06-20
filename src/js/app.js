fc424e2b7ae59dc670a74ec91dc1ede723d38c42
Matteo Sacchi <matteoseik@hotmail.it>
2022-06-20 16:20:12 +0200

    Eni first commit

--- src/js/app.js
+++ src/js/app.js
@@ -1,6 +1,3 @@
-import video1 from '../images/content/video_1.mp4';
-import video2 from '../images/content/video_2.mp4';
-import video4 from '../images/content/video_4.mp4';
 
 import './animations';
 
