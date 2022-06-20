d41bf73d5865bca853b21f43eedd7b4a97baea23
Matteo Sacchi <matteoseik@hotmail.it>
2022-06-20 17:45:42 +0200

    fix

--- src/js/animations.js
+++ src/js/animations.js
@@ -2,4 +2,15 @@ import { gsap } from "gsap";
 import { ScrollToPlugin } from "gsap/ScrollToPlugin";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
 gsap.registerPlugin(ScrollToPlugin, ScrollTrigger);
-Swiper.use([Navigation, Pagination]);
+
+
+const fadeUp = gsap.utils.toArray("[fade]");
+fadeUp.forEach((el, i) => {
+  const anim = gsap.fromTo(el, {autoAlpha: 0, y:0}, {duration: 1.5, autoAlpha: 1});
+  ScrollTrigger.create({
+    trigger: el,
+    animation: anim,
+    toggleActions: 'play none none none',
+    once: true,
+  });
+});
