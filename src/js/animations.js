01d1b8653a83f6d0fbbc6491f6434d21358b5adb
Matteo Sacchi <matteoseik@hotmail.it>
2022-06-27 14:45:23 +0200

    animation

--- src/js/animations.js
+++ src/js/animations.js
@@ -56,12 +56,14 @@ const swiper_quarter = new Swiper('.slide_quarter', {
 
 
 ScrollTrigger.batch(".card_time", {
-  interval: 0.1, // time window (in seconds) for batching to occur.
-  batchMax: 5,   // maximum batch size (targets). Can be function-based for dynamic values
-  onEnter: batch => gsap.to(batch, {opacity: 1, y: 0, stagger: {each: 0.15, grid: [1, 3]}, overwrite: true}),
-  onLeave: batch => gsap.set(batch, {opacity: 0, y: -100, overwrite: true}),
-  onEnterBack: batch => gsap.to(batch, {opacity: 1, y: 0, stagger: 0.15, overwrite: true}),
-  onLeaveBack: batch => gsap.set(batch, {opacity: 0, y: 100, overwrite: true})
+  onEnter: elements => {
+    gsap.set(elements, {transformOrigin: 'center'});
+    gsap.to(elements,
+    { opacity: 1,
+      stagger: 0.1,
+      duration: 0.2,
+    })
+  }
 });
 
 
