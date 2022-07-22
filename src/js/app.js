4ddc85d36ebbff1bcd5c59c897d79259464cda20
Matteo Sacchi <matteoseik@hotmail.it>
2022-07-22 16:47:07 +0200

    fix

--- src/js/app.js
+++ src/js/app.js
@@ -1,4 +1,6 @@
 import loop_resized from '../images/content/videoloop_resized.mp4';
+import hero_1 from '../images/content/hero_1.mp4';
+import prova_1 from '../images/content/prova_1.mp4';
 
 import './animations';
 
